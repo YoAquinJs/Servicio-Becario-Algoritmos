5b21ff79a5a8816a364253f289c4624bbd0bb18f
YoAquinJS <sebascertuche@gmail.com>
2024-04-09 18:08:14 -0600

    Added CORS bypass

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,20 @@ uvicorn main:app --reload
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from modules.config_files import ConfigFile
 from modules.credibility_matrix import load_credibility_matrix
 from modules.execute import run_executable
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
