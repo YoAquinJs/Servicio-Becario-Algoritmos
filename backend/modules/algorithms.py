36b731615c4d3b4c559ccb63d6e1688a719106d1
YoAquinJS <sebascertuche@gmail.com>
2024-05-02 21:50:12 -0600

    Algorithms refactor

--- backend/modules/algorithms.py
+++ backend/modules/algorithms.py
@@ -2,11 +2,7 @@
 
 from os import path
 from fastapi import HTTPException
-from backend.modules.base_algorithm import ExecAlgorithm
-
-OUTPUT_EXT = ".txt"
-EXEC_FILES_DIR = "Files"
-ENCODING = "utf-8"
+from modules.base_algorithm import ExecAlgorithm
 
 
 def get_algorithm(algorithm: str) -> type[ExecAlgorithm]:
@@ -22,6 +18,8 @@ def get_algorithm(algorithm: str) -> type[ExecAlgorithm]:
             raise HTTPException(status_code=404, detail=error_msg)
     return parsed_algorithm
 
+OUTPUT_EXT = ".txt"
+
 class CredibilityMatrixAlgorithm(ExecAlgorithm):
     """Representa el algoritmo de ejecucion Matrices de Credibilidad"""
     exec_config_dir = "Calculate credibility matrix"
@@ -29,17 +27,8 @@ class CredibilityMatrixAlgorithm(ExecAlgorithm):
     exec_param = 4
 
     @classmethod
-    def get_output(cls, output_type: str) -> str:
-        try:
-            output_path = path.join(EXEC_FILES_DIR,
-                                    cls.exec_config_dir,
-                                    cls.exec_output_dir,
-                                    output_type)+f"-CredibilityMatrix{OUTPUT_EXT}"
-            with open(output_path, "r", encoding=ENCODING) as file:
-                return file.read()
-        except FileNotFoundError as exc:
-            error_msg = f"matriz de credibilidad '{output_type}' no encontrada"
-            raise HTTPException(status_code=404, detail=error_msg) from exc
+    def _get_output_path(cls, output_type: str) -> str:
+        return output_type+f"-CredibilityMatrix{OUTPUT_EXT}"
 
 class SortingAlgorithm(ExecAlgorithm):
     """Representa el algoritmo de ejecucion Calculo de Sorteo"""
@@ -48,5 +37,5 @@ class SortingAlgorithm(ExecAlgorithm):
     exec_param = 5
 
     @classmethod
-    def get_output(cls, output_type: str) -> str:
-        return ""
+    def _get_output_path(cls, output_type: str) -> str:
+        return path.join(output_type, f"Assignments{OUTPUT_EXT}")
