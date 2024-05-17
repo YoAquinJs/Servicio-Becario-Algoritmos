dc875575dc16016ce3925cf5777acf65f661395c
YoAquinJS <sebascertuche@gmail.com>
2024-05-17 13:50:34 -0600

    update global host url

--- modules/backend_connection.js
+++ modules/backend_connection.js
@@ -1,7 +1,7 @@
 import { HttpError, ResponseFormatError } from "./errors.js"
 
 const LOCAL_HOST = "http://127.0.0.1:8000";
-const GLOBAL_HOST = "";
+const GLOBAL_HOST = "https://servicio-becario-algoritmos.onrender.com";
 const API_URI = window.location.hostname == "127.0.0.1" ? LOCAL_HOST : GLOBAL_HOST;
 
 //Api on start validation
